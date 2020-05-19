bcd176d055c5bc563215e8f464a28388bc82a8ce
xvzcf <xvzcf@users.noreply.github.com>
2020-05-19 15:42:21 -0400

    Paving way in oqs-scripts for BoringSSL.

--- oqs-scripts/build_liboqs.sh
+++ oqs-scripts/build_liboqs.sh
@@ -5,11 +5,14 @@
 #
 # Environment variables:
 #  - OPENSSL_DIR: path to install liboqs, default ${PROJECT_ROOT}/oqs
+#  - LIBOQS_LIBTYPE: if 'shared', build a shared library,
+#                    if 'noopenssl', build a static library without OpenSSL SHA/AES code.
+#                    else, build a static library.
 ###########
 
 set -exo pipefail
 
-OPENSSL_DIR=${OPENSSL_DIR:-"`pwd`/oqs"}
+OPENSSL_DIR=${OPENSSL_DIR:-"$(pwd)/oqs"}
 
 cd oqs-test/tmp/liboqs
 
@@ -17,9 +20,11 @@ rm -rf build
 mkdir build && cd build
 
 if [ "x${LIBOQS_LIBTYPE}" == "xshared" ]; then
-    cmake .. -GNinja -DCMAKE_INSTALL_PREFIX=${OPENSSL_DIR} -DOQS_BUILD_ONLY_LIB=ON -DBUILD_SHARED_LIBS=ON
+    cmake .. -GNinja -DCMAKE_INSTALL_PREFIX="${OPENSSL_DIR}" -DOQS_BUILD_ONLY_LIB=ON -DBUILD_SHARED_LIBS=ON
+elif [ "x${LIBOQS_LIBTYPE}" == "xnoopenssl" ]; then
+    cmake .. -GNinja -DCMAKE_INSTALL_PREFIX="${OPENSSL_DIR}" -DOQS_BUILD_ONLY_LIB=ON -DOQS_USE_OPENSSL=OFF
 else
-    cmake .. -GNinja -DCMAKE_INSTALL_PREFIX=${OPENSSL_DIR} -DOQS_BUILD_ONLY_LIB=ON ..
+    cmake .. -GNinja -DCMAKE_INSTALL_PREFIX="${OPENSSL_DIR}" -DOQS_BUILD_ONLY_LIB=ON ..
 fi
 ninja
 ninja install
