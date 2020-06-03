16f10fb9fdbb19f29aa91aa7018128aac8c0a92c
xvzcf <xvzcf@users.noreply.github.com>
2020-06-03 11:48:41 -0400

    Moving away from busy waits.

--- oqs-test/common.py
+++ oqs-test/common.py
@@ -4,7 +4,26 @@ import pathlib
 import psutil
 import time
 
-SERVER_START_TIMEOUT = 100
+key_exchanges = [
+    'oqs_kem_default', 'p256_oqs_kem_default',
+##### OQS_TEMPLATE_FRAGMENT_KEX_ALGS_START
+    # post-quantum key exchanges
+    'frodo640aes','frodo640shake','frodo976aes','frodo976shake','frodo1344aes','frodo1344shake','bike1l1cpa','bike1l3cpa','bike1l1fo','bike1l3fo','kyber512','kyber768','kyber1024','newhope512cca','newhope1024cca','ntru_hps2048509','ntru_hps2048677','ntru_hps4096821','ntru_hrss701','lightsaber','saber','firesaber','sidhp434','sidhp503','sidhp610','sidhp751','sikep434','sikep503','sikep610','sikep751','kyber90s512','kyber90s768','kyber90s1024','babybear','mamabear','papabear','babybearephem','mamabearephem','papabearephem',
+    # post-quantum + classical key exchanges
+    'p256_frodo640aes','p256_frodo640shake','p256_frodo976aes','p256_frodo976shake','p256_frodo1344aes','p256_frodo1344shake','p256_bike1l1cpa','p256_bike1l3cpa','p256_bike1l1fo','p256_bike1l3fo','p256_kyber512','p256_kyber768','p256_kyber1024','p256_newhope512cca','p256_newhope1024cca','p256_ntru_hps2048509','p256_ntru_hps2048677','p256_ntru_hps4096821','p256_ntru_hrss701','p256_lightsaber','p256_saber','p256_firesaber','p256_sidhp434','p256_sidhp503','p256_sidhp610','p256_sidhp751','p256_sikep434','p256_sikep503','p256_sikep610','p256_sikep751','p256_kyber90s512','p256_kyber90s768','p256_kyber90s1024','p256_babybear','p256_mamabear','p256_papabear','p256_babybearephem','p256_mamabearephem','p256_papabearephem',
+##### OQS_TEMPLATE_FRAGMENT_KEX_ALGS_END
+]
+signatures = [
+    'ecdsap256', 'rsa3072',
+##### OQS_TEMPLATE_FRAGMENT_SIG_ALGS_START
+    # post-quantum signatures
+    'oqs_sig_default','dilithium2','dilithium3','dilithium4','falcon512','falcon1024','mqdss3148','picnicl1fs','picnic2l1fs','qteslapi','qteslapiii',
+    # post-quantum + classical signatures
+    'p256_oqs_sig_default','rsa3072_oqs_sig_default','p256_dilithium2','rsa3072_dilithium2','p256_dilithium3','rsa3072_dilithium3','p384_dilithium4','p256_falcon512','rsa3072_falcon512','p521_falcon1024','p256_mqdss3148','rsa3072_mqdss3148','p256_picnicl1fs','rsa3072_picnicl1fs','p256_picnic2l1fs','rsa3072_picnic2l1fs','p256_qteslapi','rsa3072_qteslapi','p384_qteslapiii',
+##### OQS_TEMPLATE_FRAGMENT_SIG_ALGS_END
+]
+
+SERVER_START_ATTEMPTS = 5
 
 def run_subprocess(command, working_dir='.', expected_returncode=0, input=None):
     """
@@ -44,28 +63,33 @@ def start_server(ossl, test_artifacts_dir, sig_alg, worker_id):
     print(" > " + " ".join(command))
     server = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     server_info = psutil.Process(server.pid)
-    # Give the server SERVER_START_TIMEOUT seconds
-    # to bind to a port.
-    timeout_start = time.time()
-    while time.time() < timeout_start + SERVER_START_TIMEOUT:
+
+    # Try SERVER_START_ATTEMPTS times to see
+    # what port the server is bound to.
+    server_start_attempt = 1
+    while server_start_attempt <= SERVER_START_ATTEMPTS:
         if server_info.connections():
             break
+        else:
+            server_start_attempt += 1
+            time.sleep(3)
     server_port = str(server_info.connections()[0].laddr.port)
 
-    # Give the server SERVER_START_TIMEOUT seconds
-    # to be responsive
-    server_up = False
-    timeout_start = time.time()
-    while time.time() < timeout_start + SERVER_START_TIMEOUT:
+    # Check SERVER_START_ATTEMPTS times to see
+    # if the server is responsive.
+    server_start_attempt = 1
+    while server_start_attempt <= SERVER_START_ATTEMPTS:
         result = subprocess.run([ossl, 's_client', '-connect', 'localhost:{}'.format(server_port)],
                                 input='Q'.encode(),
                                 stdout=subprocess.PIPE,
                                 stderr=subprocess.STDOUT)
         if result.returncode == 0:
-            server_up = True
             break
+        else:
+            server_start_attempt += 1
+            time.sleep(3)
 
-    if not server_up:
+    if server_start_attempt > SERVER_START_ATTEMPTS:
         raise Exception('Cannot start OpenSSL server')
 
     return server, server_port
