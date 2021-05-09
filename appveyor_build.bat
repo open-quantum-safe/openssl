8f5d888af2786a2e51a028e6f754ec75671e8e17
Michael <57787676+baentsch@users.noreply.github.com>
2021-05-09 10:22:17 +0200

    debug build

--- appveyor_build.bat
+++ appveyor_build.bat
@@ -6,7 +6,7 @@ IF %COMPILER%==msys2 (
 IF %COMPILER%==msvc2019 (
     @echo on
     CALL "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
-    git clone --depth 1 --branch main https://github.com/open-quantum-safe/liboqs.git && cd liboqs && mkdir build && cd build && cmake .. -GNinja -DCMAKE_VERBOSE_MAKEFILE=ON -DOQS_MINIMAL_BUILD=ON -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/oqs -DOQS_BUILD_ONLY_LIB=ON -DOQS_USE_OPENSSL=OFF && ninja && ninja install && cd .. && cd ..
+    git clone --depth 1 --branch main https://github.com/open-quantum-safe/liboqs.git && cd liboqs && mkdir build && cd build && cmake .. -GNinja -DCMAKE_VERBOSE_MAKEFILE=ON -DOQS_MINIMAL_BUILD=ON -DCMAKE_INSTALL_PREFIX=%APPVEYOR_BUILD_FOLDER%/oqs -DOQS_BUILD_ONLY_LIB=ON -DOQS_USE_OPENSSL=OFF && ninja -v && ninja install && cd .. && cd ..
     set "PATH=C:\Strawberry\perl\bin;%PATH%"
     perl Configure %TARGET% %SHARED%
     perl configdata.pm --dump
