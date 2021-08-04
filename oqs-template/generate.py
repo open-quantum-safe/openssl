77e969affada87413c4d99944f9c2417d2cdd291
Douglas Stebila <dstebila@uwaterloo.ca>
2021-08-04 11:31:47 -0400

    Set NIST levels from liboqs yml datasheets

--- oqs-template/generate.py
+++ oqs-template/generate.py
@@ -67,67 +67,86 @@ def load_config(include_disabled_sigs=False):
 
     return config
 
-# As claimed_nist_level is only a runtime struct, need to compile code to get at it:
-def get_nistlevel(alg, iskem):
-    # create file to compile
-    TMPOQS_SRC="oqsnisttest.c"
-    TMPOQS_EXE="oqsnisttest"
-    with open(TMPOQS_SRC, "w") as f:
-      f.write("#include <stdio.h>\n")
-      f.write("#include <oqs/oqs.h>\n")
-      if (iskem):
-         f.write("int main(int argc, char* argv[]) { OQS_KEM *kem = OQS_KEM_new("+alg+"); printf(\"%d\", kem->claimed_nist_level); }")
-      else:
-         f.write("int main(int argc, char* argv[]) { OQS_SIG *sig = OQS_SIG_new("+alg+"); printf(\"%d\", sig->claimed_nist_level); }")
-    # now compile this so that it works with both shared and static liboqs:
-    # gcc must exist as otherwise nothing else will build afterwards
-    if os.system("cc -Ioqs/include -Loqs/lib "+TMPOQS_SRC+" -loqs -lcrypto -o "+TMPOQS_EXE) != 0:
-       print("Compilation failed. Cannot get NIST level. Exiting.")
-       exit(1)
-    # now execute it so it works for both shared and static liboqs
-    env =os.environ
-    env["LD_LIBRARY_PATH"] = "oqs/lib"
-    # in case we're trying shared OQS libs on OSX:
-    env["DYLD_LIBRARY_PATH"] = "oqs/lib"
-    p = subprocess.Popen(["./"+TMPOQS_EXE], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE, env=env)
-    output, err = p.communicate()
-    # delete temp files:
-    os.remove(TMPOQS_EXE)
-    os.remove(TMPOQS_SRC)
-    # evaluate result
-    if (p.returncode == 0):
-      return output.decode()
-    else:
-      print("Error determining NIST level for %s: %s. Exiting." % (alg, output.decode()))
-      exit(1)
+def get_kem_nistlevel(alg):
+    if 'LIBOQS_SRC_DIR' not in os.environ:
+        print("Must include LIBOQS_SRC_DIR in environment")
+        exit(1)
+    # translate family names in generate.yml to directory names for liboqs algorithm datasheets
+    if alg['family'] == 'CRYSTALS-Kyber': datasheetname = 'kyber'
+    elif alg['family'] == 'SIDH': datasheetname = 'sike'
+    elif alg['family'] == 'NTRU-Prime': datasheetname = 'ntruprime'
+    else: datasheetname = alg['family'].lower()
+    # load datasheet
+    algymlfilename = os.path.join(os.environ['LIBOQS_SRC_DIR'], 'docs', 'algorithms', 'kem', '{:s}.yml'.format(datasheetname))
+    algyml = yaml.safe_load(file_get_contents(algymlfilename, encoding='utf-8'))
+    # hacks to match names
+    def matches(name, alg):
+        def simplify(s):
+            return s.lower().replace('_', '').replace('-', '')
+        if 'FrodoKEM' in name: name = name.replace('FrodoKEM', 'Frodo')
+        if 'Saber-KEM' in name: name = name.replace('-KEM', '')
+        if '-90s' in name: name = name.replace('-90s', '').replace('Kyber', 'Kyber90s')
+        if simplify(name) == simplify(alg['name_group']): return True
+        return False
+    # find the variant that matches
+    for variant in algyml['parameter-sets']:
+        if matches(variant['name'], alg):
+            return variant['claimed-nist-level']
+    return None
+
+def get_sig_nistlevel(family, alg):
+    if 'LIBOQS_SRC_DIR' not in os.environ:
+        print("Must include LIBOQS_SRC_DIR in environment")
+        exit(1)
+    # translate family names in generate.yml to directory names for liboqs algorithm datasheets
+    if family['family'] == 'CRYSTALS-Dilithium': datasheetname = 'dilithium'
+    elif family['family'] == 'SPHINCS-Haraka': datasheetname = 'sphincs'
+    elif family['family'] == 'SPHINCS-SHA256': datasheetname = 'sphincs'
+    elif family['family'] == 'SPHINCS-SHAKE256': datasheetname = 'sphincs'
+    else: datasheetname = family['family'].lower()
+    # load datasheet
+    algymlfilename = os.path.join(os.environ['LIBOQS_SRC_DIR'], 'docs', 'algorithms', 'sig', '{:s}.yml'.format(datasheetname))
+    algyml = yaml.safe_load(file_get_contents(algymlfilename, encoding='utf-8'))
+    # hacks to match names
+    def matches(name, alg):
+        def simplify(s):
+            return s.lower().replace('_', '').replace('-', '').replace('+', '')
+        if simplify(name) == simplify(alg['name']): return True
+        return False
+    # find the variant that matches
+    for variant in algyml['parameter-sets']:
+        if matches(variant['name'], alg):
+            return variant['claimed-nist-level']
+    return None
 
 def nist_to_bits(nistlevel):
-   if nistlevel=="1" or nistlevel=="2":
+   if nistlevel==1 or nistlevel==2:
       return 128
-   if nistlevel=="3" or nistlevel=="4":
+   elif nistlevel==3 or nistlevel==4:
       return 192
-   if nistlevel=="5":
+   elif nistlevel==5:
       return 256
-   print("Unknown NIST level %s. Exiting." % (nistlevel))
-   exit(1)
+   else: 
+      return None
 
 def complete_config(config):
    for kem in config['kems']:
-      bits_level = nist_to_bits(get_nistlevel(kem['oqs_alg'], True))
+      bits_level = nist_to_bits(get_kem_nistlevel(kem))
+      if bits_level == None: 
+          print("Cannot find security level for {:s} {:s}".format(kem['family'], kem['name_group']))
+          exit(1)
       kem['bit_security'] = bits_level
    for famsig in config['sigs']:
       for sig in famsig['variants']:
-         bits_level = nist_to_bits(get_nistlevel(sig['oqs_meth'], False))
+         bits_level = nist_to_bits(get_sig_nistlevel(famsig, sig))
+         if bits_level == None: 
+             print("Cannot find security level for {:s} {:s}".format(famsig['family'], sig['name']))
+             exit(1)
          sig['security'] = bits_level
    return config
 
 config = load_config()
-# only tested OK on linux due to code compilation:
-if sys.platform=="linux":
-   config = complete_config(config)
-else:
-   print("Unsupported platform for code generation: %s. Exiting." % (sys.platform))
-   exit(1)
+config = complete_config(config)
 
 if len(sys.argv)>2: 
    # short term approach: iterate KEMs looking for OQS alg names: Argument needs to be v040 KEM KATS list
