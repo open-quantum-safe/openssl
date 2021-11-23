cb4c272e3f340c34ac3799b05e9af791525074f8
Michael <57787676+baentsch@users.noreply.github.com>
2021-11-23 12:45:42 +0100

    added generation of wireshark registry files [skip ci]

--- oqs-template/generate.py
+++ oqs-template/generate.py
@@ -112,6 +112,10 @@ populate('ssl/t1_trce.c', config, '/////')
 populate('oqs-test/common.py', config, '#####')
 populate('oqs-interop-test/common.py', config, '#####')
 
+# wireshark files
+populate('oqs-scripts/packet-tls-utils.c', config, '/////')
+populate('oqs-scripts/packet-pkcs1.c', config, '/////')
+
 config = load_config(include_disabled_sigs=True)
 populate('README.md', config, '<!---')
 
